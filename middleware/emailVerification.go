c704b759a8225415981b851201f15449744fe376
Hrishikesh-Panigrahi <hrishikesh.panigrahi38@gmail.com>
2024-11-07 15:53:00 +0530

    feat: add email verification middleware

--- middleware/emailVerification.go
+++ middleware/emailVerification.go
@@ -1,9 +1,41 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Hrishikesh-Panigrahi/StreamWatch/dbConnector"
+	"github.com/Hrishikesh-Panigrahi/StreamWatch/models"
+	"github.com/Hrishikesh-Panigrahi/StreamWatch/render"
+	"github.com/gin-gonic/gin"
+)
 
 func EmailVerification() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Code for email verification
+		cookieuser, exists := c.Get("user")
+
+		if !exists {
+			render.RenderError(c, http.StatusUnauthorized, "User not logged in. Please login to upload video.")
+			return
+		}
+
+		userID := cookieuser.(models.User).ID
+
+		fmt.Println(userID)
+
+		var user models.User
+		if err := dbConnector.DB.First(&user, userID).Error; err != nil {
+			fmt.Printf("Error retrieving User: %v\n", err)
+
+			render.RenderError(c, http.StatusInternalServerError, "An error occurred while fetching the user. Please try again later.")
+			return
+		}
+
+		if !user.Is_verified {
+			render.RenderError(c, http.StatusUnauthorized, "User not verified. Please verify your email.")
+			return
+		}
+
+		c.Next()
 	}
 }
