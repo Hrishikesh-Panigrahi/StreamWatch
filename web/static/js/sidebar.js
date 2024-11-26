9b1fb9fdc0edc276f590f12c38aa16aa4f6b154f
Hrishikesh-Panigrahi <hrishikesh.panigrahi38@gmail.com>
2024-11-26 18:27:42 +0530

    favicons

--- web/static/js/sidebar.js
+++ web/static/js/sidebar.js
@@ -3,15 +3,16 @@ const closeSidebar = document.getElementById("closeSidebar");
 const sidebar = document.getElementById("sidebar");
 
 toggleSidebar.addEventListener("click", function () {
-    if (sidebar.classList.contains("hidden")) {
-        sidebar.classList.remove("hidden");
-        sidebar.classList.add("translate-x-0");
-    } else {
-        sidebar.classList.add("hidden");
-        sidebar.classList.remove("translate-x-0");
-    }
+  if (sidebar.classList.contains("-translate-x-full")) {
+    sidebar.classList.remove("-translate-x-full");
+    sidebar.classList.add("translate-x-0");
+  } else {
+    sidebar.classList.add("-translate-x-full");
+    sidebar.classList.remove("translate-x-0");
+  }
 });
 
 closeSidebar.addEventListener("click", () => {
-    sidebar.classList.add("hidden");
+  sidebar.classList.add("-translate-x-full");
+  sidebar.classList.remove("translate-x-0");
 });
